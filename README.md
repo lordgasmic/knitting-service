628863cadefbd7594131d5345796336ba43f57e8
Nick Wellman <lordgasmic@lordgasmic.com>
2024-12-19 22:15:03 -0500

    initial

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-template-service
+# knitting-service
---
+++ src/main/java/com/lordgasmic/knittingservice/KnittingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lordgasmic.knittingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnittingServiceApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(KnittingServiceApplication.class, args);
+    }
+}
